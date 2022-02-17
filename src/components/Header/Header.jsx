cb391e2f048328b274143b6540f31df5354bc56b
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-02-16 17:17:25 -0800

    Header contains entries count

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
+import { useEntries } from '../../context/PlannerContext';
 import styles from './Header.css';
 
 export default function Header() {
+  const { entries } = useEntries();
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
-      {/* TODO: Replace hard-coded count with the actual number of entries */}
-      <span>1 Item Left</span>
+      {entries.length > 1 ? (
+        <span>{entries.length} items left</span>
+      ) : (
+        <span>{entries.length} item left</span>
+      )}
     </header>
   );
 }
